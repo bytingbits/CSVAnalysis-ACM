b24ccf0eac709219060e9c1f541a878f30b477c6
bytingbits <112688961+bytingbits@users.noreply.github.com>
2024-10-28 10:02:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,10 @@ st.write("Hey! This tool helps you quickly pull insights from our feedback forms
 dark_mode_logo = "black_mode.png"  # Replace with your dark mode logo path
 light_mode_logo = "white_mode.jpeg"  # Replace with your light mode logo path
 
-# Use the `st.beta_set_page_config` to set the theme
-#st.set_page_config(page_title="Logo Display", page_icon=light_mode_logo)
+# Create a radio button to switch between light and dark modes
+mode = st.radio("Choose mode:", options=["☀️ Light", "🌙 Dark"])
 
-# Create a toggle to switch between light and dark modes
-mode = st.selectbox("Choose mode:", ["Light", "Dark"])
-
-if mode == "Light":
+if mode == "☀️ Light":
     # Display light mode logo
     st.image(light_mode_logo, caption="Light Mode Logo")
     st.markdown('<style>body {background-color: #ffffff;}</style>', unsafe_allow_html=True)
@@ -24,4 +21,3 @@ else:
     st.markdown('<style>body {background-color: #000000; color: #ffffff;}</style>', unsafe_allow_html=True)
 
 st.write("Toggle between light and dark mode to see different logos.")
-
