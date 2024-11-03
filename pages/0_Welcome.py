33562b7ceaf55b82701d7b32d6a59003f89ed7c5
sharada <csm.sharada@gmail.com>
2024-11-03 16:17:39 +0530

    Frontend changes

---
+++ pages/0_Welcome.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+light_mode_logo = "white_mode.jpeg"  # Replace with your light mode logo path
+# Create columns for the header
+col1, col2 = st.columns([1, 5])  # Adjust the ratios as needed
+
+with col1:
+    st.image(light_mode_logo, use_column_width=True)  # Display header image
+# Set the title of the app
+with col2:
+    st.title("Welcome to ACM-CEG's Report Analyser!")
+st.write("Hey! 👋 This site is to help you make sense of our (often overwhelmingly large) Google Forms for reports and otherwise!") 
+st.write("📊 You'll need a CSV file to use this site!") 
+st.write("🧑‍💻 Once you've done that, navigate through the sidebar to create charts and tables with it! Have fun 🎉")
+
+light_mode_logo = "white_mode.jpeg"  # Replace with your light mode logo path
+st.logo(light_mode_logo)
+st.markdown("""
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.8);
+    color: black;
+    text-align: center;
+    padding: 10px;
+    border-top: 1px solid #ccc;
+    z-index: 1000;
+}
+</style>
+<div class="footer">
+    Made with ❤️ by <a href="https://github.com/bytingbits" target="_blank">sharada</a>
+</div>
+""", unsafe_allow_html=True)
